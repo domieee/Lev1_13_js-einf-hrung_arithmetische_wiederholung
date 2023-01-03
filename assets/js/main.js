cc4694dd416648064815dec61589c5972a990baf
domieee <99103235+domieee@users.noreply.github.com>
2023-01-03 16:35:18 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+// Übertrage folgende Rechenarten in deinen Code-Editor und schaue dir das Ergebnis an: 
+
+// Verwende Grundrechenarten
+// Addition
+let addition_operator = 1 + 1;
+console.log("addition: " + addition_operator);
+    // Der Additions-Operator wird genutzt um Variablen und Summen zu addieren
+
+// Subtraktion
+let subtraktion_operator = 2 - 1;
+console.log("subtraktion: " + subtraktion_operator);
+    // Der Subtraktions-Operator wird genutzt um Variablen und Summen zu subtrahieren
+
+// Multiplikation
+let multiplication_operator = 2 * 2;
+console.log("multiplication: " + multiplication_operator);
+    // Der Multiplikations-Operator wird genutzt um Variablen und Summen zu multiplizieren
+
+// / / Division
+let division_operator = 4 / 2;
+console.log("division: " + division_operator);
+    // Der Divisions-Operator wird genutzt um Variablen und Summen zu dividieren
+
+// Modul: zeigt an, was der Rest sein würde.
+let modulus_operator = 14 % 5;
+console.log("modulus: " + modulus_operator);
+/*
+    Der Modulo Wert bezieht sich auf die Summe, die übrig bleibt (Restwert), wenn die gegebenen Summen dividiert werden
+    14 % 5 => 14 / 5 = 2 * 5 = 10 > 10 - 14 = %4
+ */
